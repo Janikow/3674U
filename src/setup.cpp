06c820f1c8f1cad063dad5042904a44fdc92db3b
Janikow <160335817+Janikow@users.noreply.github.com>
2026-09-03 15:04:30 -0500

    Define CAS_MAX_ROTATION constant for motor control

--- src/setup.cpp
+++ src/setup.cpp
@@ -10,3 +10,4 @@ pros::Motor CascadeLiftMotor(7);
 
 double currentRotation = CascadeLiftMotor.get_position();
 CascadeLiftMotor.tare_position();
+const double CAS_MAX_ROTATION = 3 * 360; // 3 full rotations
